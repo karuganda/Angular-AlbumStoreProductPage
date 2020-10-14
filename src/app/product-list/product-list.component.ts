e7cda4c0876b220e11dc05692865c9488ec33deb
Karuganda Frank <41649691+karuganda@users.noreply.github.com>
2020-10-14 18:28:08 +0200

    finishing module 6

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-list',
