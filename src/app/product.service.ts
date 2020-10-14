6cc564f27a26910c7f927d70ef1377934a0be44d
Karuganda Frank <41649691+karuganda@users.noreply.github.com>
2020-10-14 16:44:00 +0200

    4 to 9

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
 
 @Injectable()
 export class ProductService {
+  pricate _albumUrl= '../assets/album.json';
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
+  getAlbum(id: number)
+  {
+    return this._http.get(this._albumUrl).map((Response)=>
+    Response.json());
+  }
 }
